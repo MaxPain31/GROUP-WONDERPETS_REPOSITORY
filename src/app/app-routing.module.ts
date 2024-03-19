8f57fb5eb1ab0fcb85f01bdaf977a0ee16a73721
Mac John Dale Laga <mac.laga11@gmail.com>
2024-03-20 00:44:20 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { MyCustomPageWithId } from './custom-page/custom-page-with-id/custom-page-with-id';
-import { AnotherPagePage } from './another-page/another-page';
-import { AuthenticationService } from './authentication.service';
-import { SharedFolder } from './shared-folder/custom-component';
 
 
 const routes: Routes = [
@@ -15,25 +11,6 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'custom-page',
-    loadChildren: () => import('./custom-page/custom-page.module').then( m => m.CustomPagePageModule)
-  },
-  {
-    path: 'custom-page/:id',
-    component: MyCustomPageWithId
-  }
-  ,
-  {
-    path: 'another-page',
-    component: AnotherPagePage,
-    canActivate: [AuthenticationService]
-  },
-  {
-    path: 'shared-folder',
-    component: SharedFolder,
-    canActivate: [AuthenticationService]
   }
 ];
 
